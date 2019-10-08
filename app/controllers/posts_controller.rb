14e02bc2c65c41d5e92c060466a9061b70aa691c
EllieChen-Git <elliechen.etc@gmail.com>
2019-10-08 13:23:36 +1100

    added methods: new & create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,24 @@
 class PostsController < ApplicationController
-    def index
+    def index                #(1)
     end
 
     def new
-        @post = Post.new      #calling Post model to create a new object
+        @post = Post.new     #(2)calling Post model to create a new object
     end
 
-    def create
+    def create                        #(4)
+        @post = Post.new(post_params)
 
+        if @post.save                 #check if post is saved: yes, go to the post; no, go to new form
+            redirect_to @post
+        else
+            render "new"
+        end
     end
+
+    private            #(3)create private-post_params: coz we'll reuse post_params in other methods
+                                   
+    def post_params           
+        params.require(:post).permit(:title, :content)  #require params from Post model, initialised them here,
+    end                                                 #so we can submit them to server
 end
